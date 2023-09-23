157c1add9bb95a3ec090d40df7a5979bee3844d5
Aham Kingsley <kingsleyaham6@gmail.com>
2023-09-23 14:37:21 +0100

    fix: fixed incorrect package name error in package.json preventing app from deploying

--- src/components/section/Timeline.jsx
+++ src/components/section/Timeline.jsx
@@ -9,7 +9,7 @@ const Timeline = () => {
           <img src={BigIdea} alt="the big idea" />
         </div>
         <div className="text-center md:text-left">
-          <div  className="flex items-center md:justify-between justify-evenly">
+          <div className="flex items-center md:justify-between justify-evenly">
             <p className="font-secondary font-bold text-[25px]">
               Introduction to getlinked <br />
               <span className="text-pink">tech Hackathon 1.0</span>
@@ -19,13 +19,11 @@ const Timeline = () => {
 
           <div className="text-[14px] pt-2 leading-8">
             <p>
-              Our tech hackathon is a melting pot of visionaries, and its
-              purpose is as clear as day: to shape the future. Whether you're a
-              coding genius, a design maverick, or a concept wizard, you'll have
-              the chance to transform your ideas into reality. Solving
-              real-world problems, pushing the boundaries of technology, and
-              creating solutions that can change the world, that's what we're
-              all about!
+              Our tech hackathon is a melting pot of visionaries, and its purpose is as clear as
+              day: to shape the future. Whether you're a coding genius, a design maverick, or a
+              concept wizard, you'll have the chance to transform your ideas into reality. Solving
+              real-world problems, pushing the boundaries of technology, and creating solutions that
+              can change the world, that's what we're all about!
             </p>
           </div>
         </div>
